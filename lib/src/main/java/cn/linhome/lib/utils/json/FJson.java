184a33b35a881f2a79d745d6942e70b01b14c2fd
ywb <nathline1029@qq.com>
2018-11-26 19:39:32 +0800

    提交api 'com.alibaba:fastjson:1.1.70.android'

--- lib/src/main/java/cn/linhome/lib/utils/json/FJson.java
+++ lib/src/main/java/cn/linhome/lib/utils/json/FJson.java
@@ -10,23 +10,23 @@ public final class FJson
     {
     }
 
-    public static <T> T json2Object(String json, Class<T> clazz)
+    public static <T> T jsonToObject(String json, Class<T> clazz)
     {
         return JSON.parseObject(json, clazz);
     }
 
-    public static String object2Json(Object obj)
+    public static String objectToJson(Object obj)
     {
         return JSON.toJSONString(obj);
     }
 
-    public static <T> T map2Object(Map map, Class<T> clazz)
+    public static <T> T mapToObject(Map map, Class<T> clazz)
     {
         T t = null;
         if (map != null)
         {
-            String json = object2Json(map);
-            t = json2Object(json, clazz);
+            String json = objectToJson(map);
+            t = jsonToObject(json, clazz);
         }
         return t;
     }
