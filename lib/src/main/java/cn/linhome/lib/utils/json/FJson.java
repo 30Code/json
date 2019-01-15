568be89fb03648b3eb076a83a889ddb250d97ead
[ywb] <nathline1029@qq.com>
2019-01-15 18:38:24 +0800

    改Gson

--- lib/src/main/java/cn/linhome/lib/utils/json/FJson.java
+++ lib/src/main/java/cn/linhome/lib/utils/json/FJson.java
@@ -1,33 +1,51 @@
 package cn.linhome.lib.utils.json;
 
-import com.alibaba.fastjson.JSON;
+import cn.linhome.lib.utils.json.adapter.DoubleAdapter;
+import cn.linhome.lib.utils.json.adapter.FloatAdapter;
+import cn.linhome.lib.utils.json.adapter.IntegerAdapter;
+import cn.linhome.lib.utils.json.adapter.LongAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Map;
 
 public final class FJson
 {
+    public static final Gson gGSON = new GsonBuilder()
+            .registerTypeAdapter(int.class, new IntegerAdapter())
+            .registerTypeAdapter(Integer.class, new IntegerAdapter())
+
+            .registerTypeAdapter(long.class, new LongAdapter())
+            .registerTypeAdapter(Long.class, new LongAdapter())
+
+            .registerTypeAdapter(float.class, new FloatAdapter())
+            .registerTypeAdapter(Float.class, new FloatAdapter())
+
+            .registerTypeAdapter(double.class, new DoubleAdapter())
+            .registerTypeAdapter(Double.class, new DoubleAdapter())
+            .create();
+
     private FJson()
     {
     }
 
     public static <T> T jsonToObject(String json, Class<T> clazz)
     {
-        return JSON.parseObject(json, clazz);
+        return gGSON.fromJson(json, clazz);
     }
 
     public static String objectToJson(Object obj)
     {
-        return JSON.toJSONString(obj);
+        return gGSON.toJson(obj);
     }
 
     public static <T> T mapToObject(Map map, Class<T> clazz)
     {
-        T t = null;
-        if (map != null)
+        if (map == null)
         {
-            String json = objectToJson(map);
-            t = jsonToObject(json, clazz);
+            return null;
         }
-        return t;
+        String json = objectToJson(map);
+        return jsonToObject(json, clazz);
     }
 }
---
+++ lib/src/main/java/cn/linhome/lib/utils/json/adapter/FloatAdapter.java
@@ -0,0 +1,18 @@
+package cn.linhome.lib.utils.json.adapter;
+
+import com.google.gson.JsonElement;
+
+public class FloatAdapter extends NumberAdapter<Float>
+{
+    @Override
+    protected Float deserializeIllegalJsonElement(JsonElement json)
+    {
+        return 0F;
+    }
+
+    @Override
+    protected Float deserializeJsonElement(JsonElement json)
+    {
+        return json.getAsFloat();
+    }
+}
